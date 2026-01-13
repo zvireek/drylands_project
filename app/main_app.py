1fe0f0b65032a9fe28f6b5e55f37c81c1c708c8b
Jasiek Zurek <339247@uwr.edu.pl>
2026-01-13 15:50:38 +0100

    na razie baloniki, a zabawa z teorią już wkrótce

--- app/main_app.py
+++ app/main_app.py
@@ -1,2 +1,17 @@
-print("hello world")
+import streamlit as st
 
+st.title("Hello Streamlit-er 👋")
+st.markdown(
+    """ 
+    This is a playground for you to try Streamlit and have fun. 
+
+    **There's :rainbow[so much] you can build!**
+
+    We prepared a few examples for you to get started. Just 
+    click on the buttons above and discover what you can do 
+    with Streamlit. 
+    """
+)
+
+if st.button("Send balloons!"):
+    st.balloons()
