094ff4dab99e6f4547731180ef2e2807cedd8b09
Jasiek Zurek <339247@uwr.edu.pl>
2026-01-24 19:31:34 +0100

    działa dyfuzja hehehe :DDDDD

--- pipeline/model.py
+++ pipeline/model.py
@@ -1,29 +1,74 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from solver import ImplicitSolver
 
 class KlausmeierModel:
-    def __init__(self, size, a, m, d1=1, d2=1, dx=0.1):
+    def __init__(self, dx, L, a, m, d1=0.025, d2=0.025):
         self.params = {'a': a, 'm': m, 'd1': d1, 'd2': d2}
-        nx, ny = size
-        self.nx, self.ny = nx, ny
+
         self.dx = dx
+        self.L = L
+        self.n = None
 
-        self.dudt = None
-        self.dvdt = None
+        self.u = None
+        self.v = None
 
-        self.u = np.ones((nx, ny))
-        self.v = np.zeros((nx, ny))
+        self.bound_indices = None
+
+        self.setup_geometry()
+
         self.time = 0.0
 
+    def setup_geometry(self):
+        """
+        Preparing the axes, grid and indices for the simulation.
+        :return:
+        """
+        # --- Defining the axes ---
+        ox = np.arange(0, self.L + self.dx, self.dx)
+        oy = np.arange(0, self.L + self.dx, self.dx)
+        self.dx = ox[1] - ox[0]
+        self.n = len(ox)
+
+        # --- Defining the grid ---
+        X, Y = np.meshgrid(ox, oy)
+
+        # --- Defining the Dirichlet boundary indices ---
+        left_ind = np.isclose(X, 0, atol = self.dx/2).flatten()
+        right_ind = np.isclose(X, self.L, atol=self.dx / 2).flatten()
+        top_ind = np.isclose(Y, self.L, atol=self.dx / 2).flatten()
+        bot_ind = np.isclose(Y, 0, atol=self.dx / 2).flatten()
+
+        self.bound_indices = {'l': left_ind, 'r': right_ind, 't': top_ind, 'b': bot_ind}
 
     def set_initial_conditions(self):
-        pass
+        n = self.n
+        self.u = np.ones((n, n)).flatten()
+        self.v = np.zeros((n, n)).flatten()
+        print("Model initial conditions set to: something")
 
     def step(self, dt, solver):
-        pass
+        self.u, self.v = solver.solve_step(self.u, self.v, self.params, dt, self.dx)
+        self.time += dt
 
-    def run_simulation(self, steps):
-        pass
+    def run_simulation(self, dt, steps, solver):
+        if self.time == 0:
+            self.set_initial_conditions()
+            solver.setup_parameters(self.n, dt, self.dx, self.bound_indices, self.params)
+
+
+        for _ in range(steps):
+            self.step(dt, solver)
 
 if __name__ == "__main__":
-    model = KlausmeierModel(size=(100, 100), a=2.0, m=0.5)
+    model = KlausmeierModel(dx=0.01, L = 1, a=2.0, m=0.5, d1 = 0.025, d2 = 0.025)
+    solver = ImplicitSolver()
+    model.run_simulation(0.1, 10, solver)
+
+    plt.imshow(model.u.reshape(model.n, model.n))
+    plt.show()
+
+    model.run_simulation(0.1, 10, solver)
+
+    plt.imshow(model.u.reshape(model.n, model.n))
+    plt.show()
--- pipeline/solver.py
+++ pipeline/solver.py
@@ -1,5 +1,77 @@
+import numpy as np
+import scipy.sparse as sp
+
+# Defining derivative matrices
+"""def D1_left(N):
+  D1 = np.eye(N, k=1) - np.eye(N)
+  return D1
+def D1_right(N):
+  D1 = -np.eye(N, k=-1) + np.eye(N)
+  return D1"""
+
+def D2(N):
+  D2_matr = -2 * np.eye(N) + np.eye(N, k=1) + np.eye(N, k=-1)
+  return D2_matr
+
+
 class ImplicitSolver:
     # to be added
 
+    def __init__(self):
+        self.dt = None
+        self.dx = None
+        self.indices = None
+        self.params = None
+        self.ev_mtr1 = None
+        self.ev_mtr2 = None
+
+    def setup_parameters(self, n, dt, dx, indices, params):
+        self.dt = dt
+        self.dx = dx
+        self.indices = indices
+        self.params = params
+        self.ev_mtr1 = self.generate_evolution_matrix(n, indices, params['d1'])
+        self.ev_mtr2 = self.generate_evolution_matrix(n, indices, params['d2'])
+        print("Solver parameters set up.")
+
+    def generate_evolution_matrix(self, n, indices, diff_coef):
+        N = n * n
+
+        # --- Identity matrices ---
+        I = np.eye(n)
+        I_N_sqr = np.eye(N)
+
+        # --- 2nd - derivative matrix ---
+        Derv2 = D2(n)
+
+        # --- Laplacian ---
+        laplacian = np.kron(I, Derv2) + np.kron(Derv2, I)
+
+        # --- Evolution matrix ---
+        factor = (diff_coef * self.dt) / (self.dx**2)
+        A = np.eye(N) - factor * laplacian
+
+        # --- Replacing the rows of evolution matrix ---
+        A[indices['l'], :] = I_N_sqr[indices['l'],]
+        A[indices['r'], :] = I_N_sqr[indices['r'],]
+        A[indices['t'], :] = I_N_sqr[indices['t'],]
+        A[indices['b'], :] = I_N_sqr[indices['b'],]
+
+        return sp.csr_matrix(A)
+
     def solve_step(self, u, v, params, dt, dx):
-        pass
+        u_tmp = u.copy()
+        v_tmp = v.copy()
+
+        # We apply the Dirichlet boundary conditions
+        for key in self.indices:
+            u_tmp[self.indices[key]] = 0
+            v_tmp[self.indices[key]] = 0
+
+        u = sp.linalg.spsolve(self.ev_mtr1, u_tmp)
+        v = sp.linalg.spsolve(self.ev_mtr2, v_tmp)
+
+        return u, v
+
+if __name__ == "__main__":
+    pass
