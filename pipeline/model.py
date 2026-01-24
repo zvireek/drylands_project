0cd28c92004bf056b1fa091bfb5b58a7ab6e55a9
Jasiek Zurek <339247@uwr.edu.pl>
2026-01-24 20:33:53 +0100

    backup zanim zmienie definicje osi na np.linspace

--- pipeline/model.py
+++ pipeline/model.py
@@ -39,12 +39,17 @@ class KlausmeierModel:
         top_ind = np.isclose(Y, self.L, atol=self.dx / 2).flatten()
         bot_ind = np.isclose(Y, 0, atol=self.dx / 2).flatten()
 
+        sum_indices = left_ind + right_ind + top_ind + bot_ind
+
+        plt.imshow(sum_indices.reshape(self.n, self.n))
+        plt.show()
+
         self.bound_indices = {'l': left_ind, 'r': right_ind, 't': top_ind, 'b': bot_ind}
 
     def set_initial_conditions(self):
         n = self.n
-        self.u = np.ones((n, n)).flatten()
-        self.v = np.zeros((n, n)).flatten()
+        self.u = (np.full((n, n), 0.70) + np.random.uniform(-0.01, 0.01, (n, n))).flatten()
+        self.v = (np.full((n, n), 1.4) + np.random.uniform(-0.01, 0.01, (n, n))).flatten()
         print("Model initial conditions set to: something")
 
     def step(self, dt, solver):
@@ -56,19 +61,27 @@ class KlausmeierModel:
             self.set_initial_conditions()
             solver.setup_parameters(self.n, dt, self.dx, self.bound_indices, self.params)
 
-
         for _ in range(steps):
             self.step(dt, solver)
 
 if __name__ == "__main__":
-    model = KlausmeierModel(dx=0.01, L = 1, a=2.0, m=0.5, d1 = 0.025, d2 = 0.025)
+
+    dt = 0.5
+    model = KlausmeierModel(dx=0.5, L = 10, a=2.0, m=1, d1 = 1, d2 = 0.01)
     solver = ImplicitSolver()
-    model.run_simulation(0.1, 10, solver)
+    model.run_simulation(dt, 2, solver)
 
-    plt.imshow(model.u.reshape(model.n, model.n))
-    plt.show()
+    """fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(model.u.reshape(model.n, model.n))
+    ax[1].imshow(model.v.reshape(model.n, model.n))
+    # plt.colorbar()
+    fig.show()
 
-    model.run_simulation(0.1, 10, solver)
 
-    plt.imshow(model.u.reshape(model.n, model.n))
-    plt.show()
+    model.run_simulation(dt, 100, solver)
+
+    fig2, ax2 = plt.subplots(1, 2)
+    ax2[0].imshow(model.u.reshape(model.n, model.n))
+    ax2[1].imshow(model.v.reshape(model.n, model.n))
+    # plt.colorbar()
+    fig2.show()"""
