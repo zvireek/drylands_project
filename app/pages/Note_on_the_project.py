529e7c9fb5f1e02b5b6ed59a943b06c5614fe035
Jasiek Żurek <339247@uwr.edu.pl>
2026-02-15 12:59:20 +0100

    klaryfikacja

--- app/pages/Note_on_the_project.py
+++ app/pages/Note_on_the_project.py
@@ -12,4 +12,7 @@ st.markdown(
 st.subheader("AI use disclaimer")
 
 st.write("Throughout this project i used AI in debugging the code, making my plots look nicer, "
-         "and rewriting my handwritten notes into latex of which results you can see on **1. Theoretical introduction** and **3. Stability analysis.**")
+         "and rewriting my handwritten notes into latex of which results you can see on **1. Theoretical introduction** and **3. Stability analysis.**\n")
+
+st.success("However I would like to stress that I did not use AI to solve any of the problems, write any of the code for simulations which was a purpose of this project, "
+         "or make any of the conclusions.")
